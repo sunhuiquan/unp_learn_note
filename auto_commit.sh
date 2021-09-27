889dc98d2b82948c2f4156f42bbd8194cfed3e67
sunhuiquan <sunhuiquan@yeah.net>
2021-09-27 17:07:23 +0800

    add

--- auto_commit.sh
+++
@@ -1,5 +0,0 @@
-#/bin/sh
-
-git add /home/water/work/unp_learn_note # 注意这个是以执行的进程的当前路径为基准，所以要用绝对地址
-git commit -m "add"
-git push
--- exercise/ch6/demo.c
+++
@@ -1,8 +0,0 @@
-#include <sys/select.h>
-
-int main()
-{
-	fd_set rfd;
-
-	return 0;
-}
