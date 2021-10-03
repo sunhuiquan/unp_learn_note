629446a0199e866dcc1efb8c1ccaebd0d1b6d43a
sunhuiquan <sunhuiquan@yeah.net>
2021-10-03 15:35:18 +0800

    add

--- exercise/ch8/6/sv2.c
+++ exercise/ch8/6/sv2.c
@@ -4,11 +4,8 @@ int main()
 {
 	int sfd, readn;
 	struct sockaddr_in addr;
-	struct sockaddr_in from_addr;
 	struct sockaddr_in conn_addr;
-	socklen_t len;
 	char data[MAXLINE];
-	char buf[MAXLINE];
 
 	if ((sfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
 		err_sys("socket");
@@ -23,31 +20,32 @@ int main()
 
 	bzero(&conn_addr, sizeof(conn_addr));
 	conn_addr.sin_family = AF_INET;
-	conn_addr.sin_port = htons(50000);
-	if (inet_pton(AF_INET, "127.0.0.1", &conn_addr.sin_addr) == -1)
+	conn_addr.sin_port = htons(50001);
+	if (inet_pton(AF_INET, "172.28.33.22", &conn_addr.sin_addr) == -1)
 		err_sys("inet_pton");
 	if (connect(sfd, (struct sockaddr *)&conn_addr, sizeof(conn_addr)) == -1)
 		err_sys("connect");
 
-	len = sizeof(from_addr);
-	if ((readn = recvfrom(sfd, data, MAXLINE, 0, (struct sockaddr *)&from_addr, &len)) < 0)
-		err_sys("recvfrom");
+	if (write(sfd, "a", 1) != 1)
+		err_sys("write");
+
+	if ((readn = read(sfd, data, MAXLINE)) < 0)
+		err_sys("read");
 
 	if (write(STDOUT_FILENO, data, readn) != readn)
 		err_sys("write");
-	if (inet_ntop(AF_INET, &from_addr.sin_addr, buf, MAXLINE) == NULL)
-		err_sys("inet_ntop");
-	printf("\nfrom ip:%s\n", buf);
 
-	len = sizeof(from_addr);
-	if ((readn = recvfrom(sfd, data, MAXLINE, 0, (struct sockaddr *)&from_addr, &len)) < 0)
-		err_sys("recvfrom");
+	if ((readn = read(sfd, data, MAXLINE)) < 0)
+		err_sys("read");
 
 	if (write(STDOUT_FILENO, data, readn) != readn)
 		err_sys("write");
-	if (inet_ntop(AF_INET, &from_addr.sin_addr, buf, MAXLINE) == NULL)
-		err_sys("inet_ntop");
-	printf("\nfrom ip:%s\n", buf);
+
+	// if ((readn = read(sfd, data, MAXLINE)) < 0)
+	// 	err_sys("read");
+
+	// if (write(STDOUT_FILENO, data, readn) != readn)
+	// 	err_sys("write");
 
 	return 0;
 }
